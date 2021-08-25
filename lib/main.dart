7f4008cc1061120c01c7c4d97df294519a0aae44
kherld-hussein <eternety11@gmail.com>
2021-08-25 06:52:08 +0300

    Separate multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+import 'package:quran_hadith/controller/favorite.dart';
 import 'package:quran_hadith/controller/quranAPI.dart';
 import 'package:quran_hadith/screens/home_screen.dart';
 import 'package:quran_hadith/theme/theme_state.dart';
@@ -11,11 +10,20 @@ import 'package:quran_hadith/theme/theme_state.dart';
 import 'theme/app_theme.dart';
 
 final quranApi = QuranAPI();
-final themeState = ThemeState();
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(QuranHadith());
+  Get.lazyPut<ThemeState>(() => ThemeState());
+  ThemeState.to.getThemeModeFromPreferences();
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(create: (context) => quranApi),
+        ChangeNotifierProvider.value(value: OnFavorite()),
+      ],
+      child: QuranHadith(),
+    ),
+  );
 }
 
 // Audio url: https://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/1
@@ -27,7 +35,7 @@ class QuranHadith extends StatefulWidget {
 }
 
 class _QuranHadithState extends State<QuranHadith> {
-  Locale? localeCallback(locale, supportedLocales) {
+  Locale localeCallback(locale, supportedLocales) {
     for (var supportedLocale in supportedLocales) {
       if (supportedLocale.languageCode == locale.languageCode &&
           supportedLocale.countryCode == locale.countryCode)
@@ -38,34 +46,22 @@ class _QuranHadithState extends State<QuranHadith> {
 
   @override
   Widget build(BuildContext context) {
-    final isPlatformDark =
-        WidgetsBinding.instance!.window.platformBrightness == Brightness.dark;
-    final initTheme = isPlatformDark ? darkTheme : theme;
-    return ThemeProvider(
-      initTheme: initTheme,
-      child: MultiProvider(
-        providers: [
-          Provider(create: (context) => quranApi),
-          // Provider(create: (context) => themeState),
-        ],
-        child: GetMaterialApp(
-          localizationsDelegates: [
-            // AppLocale.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-          ],
-          // localeResolutionCallback: localeCallback,
-          supportedLocales:
-              QuranHadith.supportedLocales.map((l) => Locale(l, '')).toList(),
-          title: 'Qur’ān Hadith',
-          darkTheme: darkTheme,
-          themeMode: ThemeState.isDarkMode ? ThemeMode.dark : ThemeMode.light,
-          theme: theme,
-          home: HomeScreen(),
-          debugShowCheckedModeBanner: false,
-        ),
-      ),
+    return GetMaterialApp(
+      localizationsDelegates: [
+        // AppLocale.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      localeResolutionCallback: localeCallback,
+      supportedLocales:
+          QuranHadith.supportedLocales.map((l) => Locale(l, '')).toList(),
+      title: 'Qur’ān Hadith',
+      darkTheme: darkTheme,
+      themeMode: ThemeState.to.themeMode!,
+      theme: theme,
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
