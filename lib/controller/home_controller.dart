018de2d968155441c9e3b65a4bb0bba33e2368cd
kherld-hussein <eternety11@gmail.com>
2021-08-25 06:49:08 +0300

    Remove unrelated files

--- lib/controller/home_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:quran_hadith/controller/base_controller.dart';
-
-class HomeController extends BaseController {
-  static HomeController get to => Get.find();
-  final searchFocusNode = FocusNode();
-}
