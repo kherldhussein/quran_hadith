29e48b67530a86571ca7f4bfe30ed635a349314f
kherld-hussein <eternety11@gmail.com>
2023-03-01 20:28:52 +0300

    Add custom support btn

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:popover/popover.dart';
 import 'package:quran_hadith/layout/adaptive.dart';
 
+import '../theme/app_theme.dart';
 import 'menu_list_items.dart';
 
 class RoundCustomButton extends StatelessWidget {
@@ -34,6 +35,46 @@ class RoundCustomButton extends StatelessWidget {
             },
             splashRadius: 1,
             icon: FaIcon(icon),
+            color: Colors.black),
+      ),
+    );
+  }
+}
+
+class RoundCustomButton2 extends StatelessWidget {
+  final List<Widget>? children;
+  final IconData? icon;
+
+  const RoundCustomButton2({Key? key, this.icon, this.children})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isSmall = isDisplayVerySmallDesktop(context);
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Container(
+        constraints: BoxConstraints(minWidth: 0),
+        width: 80,
+        decoration: BoxDecoration(
+          // border: Border.all(color: Color(0xffeef2f5)),
+          color: kDarkSecondaryColor,
+          borderRadius: BorderRadius.circular(isSmall ? 30 : 50),
+        ),
+        child: IconButton(
+            onPressed: () {
+              showPopover(
+                  width: 250,
+                  height: 265,
+                  context: context,
+                  backgroundColor: Theme.of(context).canvasColor,
+                  bodyBuilder: (context) => ListItems(children: children));
+            },
+            splashRadius: 1,
+            icon: Text(
+              'Support',
+              style: TextStyle(letterSpacing: 2),
+            ),
             color: Colors.black),
       ),
     );
