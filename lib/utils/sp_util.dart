43be2a804b9088adadbba4f39be32c5ae3b40d57
kherld-hussein <eternety11@gmail.com>
2023-04-24 02:07:29 +0300

    Add user preference

--- lib/utils/sp_util.dart
+++ lib/utils/sp_util.dart
@@ -1,6 +1,15 @@
 import 'package:quran_hadith/utils/shared_p.dart';
+
 class SpUtil {
   static String isFavorite = 'FAVORITE';
+  static String user = 'user';
+
   static bool? getFavorite() => appSP.getBool(isFavorite);
-  static Future<bool> setFavorite(bool value) => appSP.setBool(isFavorite, value);
+
+  static String? getUser() => appSP.getString(user);
+
+  static Future<bool> setFavorite(bool value) =>
+      appSP.setBool(isFavorite, value);
+
+  static Future<bool> setUser(bool value) => appSP.setBool(user, value);
 }
