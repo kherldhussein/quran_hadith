c1aba0a6a8d8ba49e2ceadd6d8b184dbbf2060a6
kherld-hussein <eternety11@gmail.com>
2023-04-25 19:24:52 +0300

    Implement data cache from server

--- lib/controller/quranAPI.dart
+++ lib/controller/quranAPI.dart
@@ -1,28 +1,34 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'package:quran_hadith/models/juzModel.dart';
 import 'package:quran_hadith/models/surahModel.dart';
-// import 'package:dio/dio.dart';
-// import 'package:dio_http_cache/dio_http_cache.dart';
+
+import 'package:dio_http_cache/dio_http_cache.dart';
+
 /// The Qur’ān contains 6236 verses
 class QuranAPI {
-  // late Response response;
-  // Dio dio = Dio();
+  late Response response;
+  Dio dio = Dio();
+
   Future<SurahsList> getSuratList() async {
-    // dio.interceptors.add(DioCacheManager(CacheConfig(baseUrl: 'http://api.alquran.cloud/v1/quran/quran-uthmani')));
     String url = "http://api.alquran.cloud/v1/quran/quran-uthmani";
-    final response = await http.get(Uri.parse(url));
+    response = await dio.get(
+      url,
+      options:
+          buildCacheOptions(Duration(days: 7), maxStale: Duration(days: 10)),
+    );
     if (response.statusCode == 200) {
-      return SurahsList.fromJSON(json.decode(response.body));
+      return SurahsList.fromJSON(json.decode(response.data));
     } else {
       throw Exception("Failed to Get Data");
     }
   }
 
   Future<SurahsList> getSurahListAssets(int index) async {
-    final response = await rootBundle.loadString('assets/surah/surah_$index.json');
+    final response =
+        await rootBundle.loadString('assets/surah/surah_$index.json');
     var res = json.decode(response);
     var data = res['$index'];
     return SurahsList.fromJSON(data);
@@ -36,9 +42,9 @@ class QuranAPI {
 
   Future<JuzModel> getJuzz({required int index}) async {
     String url = "http://api.alquran.cloud/v1/juz/$index/quran-uthmani";
-    final response = await http.get(Uri.parse(url));
+    response = await dio.get(url);
     if (response.statusCode == 200) {
-      return JuzModel.fromJSON(json.decode(response.body));
+      return JuzModel.fromJSON(json.decode(response.data));
     } else {
       throw Exception("Failed to Get Data");
     }
@@ -46,9 +52,9 @@ class QuranAPI {
 
   Future<SurahsList> getSearch({required String keyWord}) async {
     String url = "http://api.alquran.cloud/v1/search/$keyWord/all/en";
-    final response = await http.get(Uri.parse(url));
+    response = await dio.get(url);
     if (response.statusCode == 200) {
-      return SurahsList.fromJSON(json.decode(response.body));
+      return SurahsList.fromJSON(json.decode(response.data));
     } else {
       print("Failed to load");
       throw Exception("Failed to Get Data");
@@ -57,9 +63,9 @@ class QuranAPI {
 
   Future<SurahsList> getSuratAudio({required String suratNo}) async {
     String url = "http://api.alquran.cloud/v1/surah/$suratNo/en.ahmedali";
-    final response = await http.get(Uri.parse(url));
+    response = await dio.get(url);
     if (response.statusCode == 200) {
-      return SurahsList.fromJSON(json.decode(response.body));
+      return SurahsList.fromJSON(json.decode(response.data));
     } else {
       throw Exception("Failed to Get Data");
     }
@@ -67,9 +73,9 @@ class QuranAPI {
 
   Future<Ayah> getAyaAudio({required int ayaNo}) async {
     String url = "https://cdn.alquran.cloud/media/audio/ayah/Hani Rifai/$ayaNo";
-    final response = await http.get(Uri.parse(url));
+    response = await dio.get(url);
     if (response.statusCode == 200) {
-      return Ayah.fromJSON(json.decode(response.body));
+      return Ayah.fromJSON(json.decode(response.data));
     } else {
       throw Exception("Failed to Get Data");
     }
