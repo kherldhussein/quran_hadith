33a445f0f730610d389318328b64c0e33f815934
Kh3rld <kherld.hussein@gmail.com>
2025-10-06 12:04:51 -0400

    Update Linux main entry point

--- linux/main.cc
+++ linux/main.cc
@@ -1,6 +1,14 @@
 #include "my_application.h"
 
+#include <clocale>
+#include <cstdlib>
+
 int main(int argc, char** argv) {
+  // Force numeric locale to "C" so audio libraries relying on decimal parsing
+  // work consistently regardless of the user's environment.
+  setenv("LC_NUMERIC", "C", /*overwrite=*/0);
+  setlocale(LC_NUMERIC, "C");
+
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
